1e12c6e8b2c2088b221ced37bc0be7a88e0e5a50
Danilo Patrial <danmcamposp@gmail.com>
2025-05-29 20:19:44 -0300

    Reduced readme image size.

--- scripts/rescale.py
+++ scripts/rescale.py
@@ -5,10 +5,15 @@ import os
 
 Image.MAX_IMAGE_PIXELS = None
 
-def rescale(image_path: str, output_path: str = '..\\images', size: tuple = (3840, 2160)) -> None:
+_8k:    tuple = (7680, 4320)
+_4k:    tuple = (3840, 2160)
+_1080p: tuple = (1920, 1080)
+_720p:  tuple = (1280, 720 )
+
+def rescale(image_path: str, output_path: str = '..\\images', size: tuple = _4k) -> None:
     image = Image.open(image_path)
     rescaled = image.resize(size)
     rescaled.save(os.path.join(output_path, os.path.basename(image_path)))
 
 if __name__ == '__main__':
-    rescale('..\\renders\\plate_carree_bp_rp_to_kelvin_coloring.png')
+    rescale('..\\renders\\plate_carree_bp_rp_to_kelvin_coloring.png', size=_1080p)
