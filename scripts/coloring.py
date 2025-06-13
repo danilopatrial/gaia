26145f787859bd990891ecd8872a8d223339fee7
Danilo Patrial <danmcamposp@gmail.com>
2025-06-13 05:22:44 -0300

    Fix parallax brightness scales

--- scripts/coloring.py
+++ scripts/coloring.py
@@ -28,29 +28,36 @@ def bp_rp_to_rgb(bp_rp: float) -> Tuple[int, int, int]:
     return rgb
 
 
+from typing import Tuple
+import numpy as np
+from numba import njit
+
 @njit
 def bp_rp_parallax_to_rgb(bp_rp: float, parallax: float) -> Tuple[int, int, int]:
     if bp_rp < -0.4: bp_rp = -0.4
     if bp_rp > 4.0: bp_rp = 4.0
 
-    temp_kelvin = 8700 / (bp_rp + 0.55)
-    temp = min(40000, max(1000, temp_kelvin)) / 100.0
+    temp_kelvin = 8700.0 / (bp_rp + 0.55)
+    temp = min(40000.0, max(1000.0, temp_kelvin)) / 100.0
 
-    if temp <= 66:
+    if temp <= 66.0:
         red = 255.0
-        green = 99.4708025861 * math.log(temp) - 161.1195681661
-        blue = 0.0 if temp <= 19 else 138.5177312231 * math.log(temp - 10) - 305.0447927307
+        green = 99.4708025861 * np.log(temp) - 161.1195681661
+        if temp <= 19.0:
+            blue = 0.0
+        else:
+            blue = 138.5177312231 * np.log(temp - 10.0) - 305.0447927307
     else:
         red = 329.698727446 * ((temp - 60.0) ** -0.1332047592)
         green = 288.1221695283 * ((temp - 60.0) ** -0.0755148492)
         blue = 255.0
 
-    if parallax < 0:
+    if parallax > 0.0:
         distance_pc = 1000.0 / parallax
     else:
         distance_pc = 1e6
 
-    brightness_scale = 1.0 / (0.1 + (distance_pc / 100.0) ** 2)
+    brightness_scale = 1.0 / (1.0 + (distance_pc / 100.0))
     brightness_scale = min(1.0, brightness_scale)
 
     red   = min(255, max(0, int(red   * brightness_scale)))
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
 from projections import mollweide, hammer_aitoff, plate_carree
-from coloring    import bp_rp_to_rgb
+from coloring    import bp_rp_to_rgb, bp_rp_parallax_to_rgb
 from utils       import basic_log_config
 from render      import render
 from typing      import Callable, Tuple
@@ -16,8 +16,8 @@ if __name__ == '__main__' and sys.argv[1] == '-r':
 
     width, height = 38400, 21600                                                                                #
 
-    coo_func: Callable[..., Tuple] = hammer_aitoff  # Projections (i.e. Mollweide, Hammer-Aitoff, Plate Carrée) #
-    rgb_func: Callable[..., Tuple] = bp_rp_to_rgb   # Coloring (i.e. BP-RP to RGB (temperature), )              #
+    coo_func: Callable[..., Tuple] = plate_carree   # Projections (i.e. Mollweide, Hammer-Aitoff, Plate Carrée) #
+    rgb_func: Callable[..., Tuple] = bp_rp_parallax_to_rgb   # Coloring (i.e. BP-RP to RGB (temperature), )     #
 
     img_name: str = f'{coo_func.__name__}_{rgb_func.__name__}'
     render_path: str = '..\\renders\\{}.png'.format(img_name)
@@ -32,7 +32,7 @@ if __name__ == '__main__' and sys.argv[1] == '-r':
         coo_func=coo_func,
         coo_args_names=('ra', 'dec'),                                                                           #
         rgb_func=rgb_func,
-        rgb_args_names=('bp_rp',),                                                                              #
+        rgb_args_names=('bp_rp', 'parallax'),                                                                   #
         render_path=render_path,
         width=width,
         height=height
